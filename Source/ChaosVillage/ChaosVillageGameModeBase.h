e0859891cf715e7ab04873bfa1ad0288b30dc20d
Alex T <trulovecoding@gmail.com>
2023-05-30 10:10:00 -0400

    Actual first puch

---
+++ Source/ChaosVillage/ChaosVillageGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ChaosVillageGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAOSVILLAGE_API AChaosVillageGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ChaosVillage/TestPushFile.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TestPushFile.h"
+
+TestPushFile::TestPushFile()
+{
+}
+
+TestPushFile::~TestPushFile()
+{
+}
